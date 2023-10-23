06947cd61756216ebb397338a88e835e280ab967
yuchul <drivegod5@naver.com>
2023-10-23 14:39:13 +0900

    하노이타워게임 코드 완성

--- TowerOfHanoi.py
+++ TowerOfHanoi.py
@@ -29,3 +29,92 @@ def main():
     큰 원판이 있으며, 유효하지 않은 구성이다. 리스트[3,1]이 허용되는 이유는
     작은 원판이 큰 원판 상단에 올라갈 수 있기 때문이다."""
     towers = {"A": copy.copy(SOLVED_TOWER), "B": [], "C": []}
+
+    while True:  # 이 루프문이 한 번 순회할 때마다 한 턴을 진행한다
+        # 탑과 원판을 표시한다
+        displayTowers(towers)
+
+        # 사용자에게 이동 명령을 요청한다
+        fromTower, toTower = getPlayerMove(towers)
+
+        # 맨 위 원판을 fromTower에서 toTower로 이동한다
+        disk = towers[fromTower].pop()
+        towers[toTower].append(disk)
+
+        # 사용자가 퍼즐을 풀었는지 확인한다
+        if SOLVED_TOWER in (towers["B"], towers["C"]):
+            displayTowers(towers)  # 마지막으로 탑을 한 번 더 표시한다
+            print("퍼즐을 풀었습니다! 참 잘했습니다!")
+            sys.exit()
+
+
+def getPlayerMove(towers):
+    """플레이어에게 이동 명령을 요청한다. (fromTower, toTower)를 반복한다."""
+
+    while True:  # 플레이어가 유효한 이동 명령을 입력할 때까지 계속 요청한다.
+        print('탑의 "시작"과 "끝"의 글자 또는 QUIT를 입력하십시오.')
+        print("(예: 탑 A에서 탑 B로 원판을 이동하려면 AB를 입력합니다.)")
+        print()
+        response = input("> ").upper().strip
+
+        if response == "QUIT":
+            print("즐겁게 퍼즐을 풀어주셔서 감사합니다!")
+            sys.exit
+
+        # 사용자가 유효한 탑 문자를 입력했는지 확인한다
+        if response not in ("AB", "AC", "BA", "BC", "CA", "CB"):
+            print("AB, AC, BA, BC, CA, CB 중 하나를 입력하십시오.")
+            continue  # 플레이어에게 이동 명령을 다시 요청한다.
+
+        # 더 설명적인 변수 이름을 사용한다
+        fromTower, toTower = response[0], response[1]
+
+        if len(towers[fromTower]) == 0:
+            # from 탑은 비어 있을 수 없다
+            print("원판이 없는 탑을 선택했습니다.")
+            continue  # 플레이어에게 이동 명령을 다시 요청한다
+        elif len(towers[toTower]) == 0:
+            # 어떤 원판이라도 빈 "to" 탑으로 이동이 가능하다
+            return fromTower, toTower
+        elif towers[toTower][-1] < towers[fromTower][-1]:
+            print("더 작은 원판에 더 큰 원판을 올릴 수 없습니다.")
+            continue  # 플레이어에게 이동 명령을 다시 요청한다
+        else:
+            # 유효한 움직임이므로 선택된 탑을 반환한다
+            return fromTower, toTower
+
+
+def displayTower(towers):
+    """세 탑에 배치된 원판을 표시한다."""
+
+    # 세 탑을 표시한다
+    for level in range(TOTAL_DISKS, -1, -1):
+        for tower in (towers["A"], towers["B"], towers["C"]):
+            if level >= len(tower):
+                displayDisk(0)  # 원판이 없는 빈 기둥을 표시한다
+            else:
+                displayDisk(tower[level])  # 원판을 표시한다
+        print()
+
+    # 탑 이름 A, B, C를 표시한다
+    emptySpace = " " * (TOTAL_DISKS)
+    print("{0} A{0}{0} B{0}{0} C\n".format(emptySpace))
+
+
+def displayDisk(width):
+    """주어진 width로 원판을 표시한다. width가 0이면 원판이 없음을 의미한다."""
+    emptySpace = " " * (TOTAL_DISKS - width)
+
+    if width == 0:
+        # 원판이 없는 기둥을 표시한다
+        print(f"{emptySpace}||{emptySpace}", end="")
+    else:
+        # 원판을 표시한다
+        disk = "@" * width
+        numLabel = str(width).rjust(2, "_")
+        print(f"{emptySpace}{disk}{numLabel}{disk}{emptySpace}", end="")
+
+
+# 이 프로그램이 (임포트되지 않고) 단독으로 실행되면, 게임을 시작한다
+if __name__ == "__main__":
+    main()
